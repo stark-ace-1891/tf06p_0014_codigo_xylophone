db835c6ad798d415c47a4b3066fad5ee235d2cf8
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-04 23:37:51 -0500

    Modificacion inicial en blanco

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
